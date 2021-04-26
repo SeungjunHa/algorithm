d871196a9ac4cc80d1b745448a5c5d44450c4714
SeungjunHa <gkak1121@naver.com>
2021-04-26 18:11:56 +0900

    matrix_rotate, strtok_r

--- a.cpp
+++ a.cpp
@@ -4,10 +4,17 @@
 #include <queue>
 #include <string>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
+#define likely(x) __builtin_expect((x), 1)
+#define unlikely(x) __builtin_expect((x), 0)
+
 int main(void)
 {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	return 0;
 }
