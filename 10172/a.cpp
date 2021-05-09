e9202a361b894afc21fe80f27788c04d8952bbb3
SeungjunHa <gkak1121@naver.com>
2021-05-10 03:11:51 +0900

    dog

---
+++ 10172/a.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+#define likely(x) __builtin_expect((x), 1)
+#define unlikely(x) __builtin_expect((x), 0)
+
+int main(void)
+{
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+
+	return 0;
+}
